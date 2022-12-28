cfd27ee4c8856d9285be8c854f2b02af5c05dfb3
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2022-12-27 20:14:49 -0400

    Se generaron los modelos a partir de la base de datos

--- app/Http/Controllers/ProgramaController.php
+++ app/Http/Controllers/ProgramaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Programa;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,7 +12,8 @@ class ProgramaController extends Controller
     //
     public function index()
     {
-        $programas = DB::table('programa')->get();
+        //        $programas = DB::table('programa')->get();
+        $programas = Programa::get();
         return view('content.pages.pages-programa', ['listaProgramas' => $programas]);
     }
 }
---
+++ app/Models/HorarioCurso.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class HorarioCurso
+ * 
+ * @property int $hora_curs_id
+ * @property string $hora_curs_dia
+ * @property time without time zone $hora_curs_hini
+ * @property time without time zone $hora_curs_hfin
+ * @property int $grup_curs
+ * 
+ * @property GrupoCurso $grupo_curso
+ *
+ * @package App\Models
+ */
+class HorarioCurso extends Model
+{
+	protected $table = 'horario_curso';
+	protected $primaryKey = 'hora_curs_id';
+	public $timestamps = false;
+
+	protected $casts = [
+		'hora_curs_hini' => 'time without time zone',
+		'hora_curs_hfin' => 'time without time zone',
+		'grup_curs' => 'int'
+	];
+
+	protected $fillable = [
+		'hora_curs_dia',
+		'hora_curs_hini',
+		'hora_curs_hfin',
+		'grup_curs'
+	];
+
+	public function grupo_curso()
+	{
+		return $this->belongsTo(GrupoCurso::class, 'grup_curs');
+	}
+}
