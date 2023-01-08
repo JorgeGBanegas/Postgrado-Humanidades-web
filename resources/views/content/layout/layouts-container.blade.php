a00bc6db87180b600859f50bae94439d9eda2983
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2023-01-08 17:00:52 -0400

    Nuevo Menu

--- resources/views/content/layout/layouts-container.blade.php
+++ resources/views/content/layout/layouts-container.blade.php
@@ -1,24 +1,11 @@
-@php
-$container = 'container-xxl';
-$containerNav = 'container-xxl';
-@endphp
+@extends('layouts.sections.menu.burguerMenu')
 
-@extends('layouts/contentNavbarLayout')
+@section('title', 'Home')
 
-@section('title', 'Container - Layouts')
-
-@section('content')
-<!-- Layout Demo -->
+@section('content-body')
 <div class="layout-demo-wrapper">
   <div class="layout-demo-placeholder">
     <img src="{{asset('assets/img/logo/postgradoHumanidades.jpg')}}" class="img-fluid" alt="Layout container">
   </div>
-  <div class="layout-demo-info">
-    <h4>Layout container</h4>
-    <p>Container layout sets a <code>max-width</code> at each responsive breakpoint.</p>
-  </div>
 </div>
-<!--/ Layout Demo -->
-
-
 @endsection
